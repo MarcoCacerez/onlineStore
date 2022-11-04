3eda5061a21b533095e5aa70679b0c71b86d1f1f
Marco Aldair de Jesus Caceres <l18390579@chetumal.tecnm.mx>
2022-11-04 18:31:02 -0500

    Actualizacion

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    $viewData = [];
-    $viewData["title"] = "Home Page - Online Store";
-    return view('home.index')->with("viewData", $viewData);
-});
-Route::get('/about', 'App\Http\Controllers\HomeController@about')->name("home.about");
+Route::get('/','App\Http\Controllers\HomeController@index')->name("home.index");
+Route::get('/about','App\Http\Controllers\HomeController@about')->name("home.about");
+Route::get('/products','App\Http\Controllers\ProductController@index')->name("product.index");
+Route::get('/products/{id}','App\Http\Controllers\ProductController@show')->name("product.show");
+Route::get('/admin', 'App\Http\Controllers\Admin\AdminHomeController@index')->name("admin.home.index");
